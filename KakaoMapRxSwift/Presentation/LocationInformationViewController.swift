3321454fb1e2d93834afb0e1c9b06e3068daabd5
TDCIAN <tdcian71@gmail.com>
2022-04-21 08:53:01 +0900

    config locationinformation viewcontroller and viewmodel

---
+++ KakaoMapRxSwift/Presentation/LocationInformationViewController.swift
@@ -0,0 +1,151 @@
+//
+//  ViewController.swift
+//  KakaoMapRxSwift
+//
+//  Created by JeongminKim on 2022/04/21.
+//
+
+import UIKit
+import CoreLocation
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class LocationInformationViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    
+    let locationManager = CLLocationManager()
+    let mapView = MTMapView()
+    let detailListTableView = UITableView()
+    let currentLocationButton = UIButton()
+    let viewModel = LocationInformationViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mapView.delegate = self
+        locationManager.delegate = self
+        
+        bind(viewModel)
+        attribute()
+        layout()
+    }
+    
+    func bind(_ viewModel: LocationInformationViewModel) {
+        viewModel.setMapCenter
+            .emit(to: mapView.rx.setMapCenterPoint)
+            .disposed(by: disposeBag)
+        
+        // 뷰모델에서 에러 메시지를 받을 때마다 얼럿 컨트롤러를 띄운다
+        viewModel.errorMessage
+            .emit(to: self.rx.presentAlert)
+            .disposed(by: disposeBag)
+        
+        currentLocationButton.rx.tap
+            .bind(to: viewModel.currentLocationButtonTapped)
+            .disposed(by: disposeBag)
+    }
+    
+    private func attribute() {
+        title = "내 주변 편의점 찾기"
+        view.backgroundColor = .systemBackground
+        mapView.currentLocationTrackingMode = .onWithoutHeadingWithoutMapMoving
+        currentLocationButton.setImage(UIImage(systemName: "location.fill"), for: .normal)
+        currentLocationButton.backgroundColor = .white
+        currentLocationButton.layer.cornerRadius = 20
+    }
+    
+    private func layout() {
+        [mapView, currentLocationButton, detailListTableView]
+            .forEach { view.addSubview($0) }
+        
+        mapView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalTo(view.snp.centerY).offset(100)
+        }
+        
+        currentLocationButton.snp.makeConstraints {
+            $0.bottom.equalTo(detailListTableView.snp.top).offset(-12)
+            $0.leading.equalToSuperview().offset(12)
+            $0.width.height.equalTo(40)
+        }
+        
+        detailListTableView.snp.makeConstraints {
+            $0.centerX.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(8)
+            $0.top.equalTo(mapView.snp.bottom)
+        }
+    }
+}
+
+extension LocationInformationViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse, .notDetermined:
+            return
+        default:
+            viewModel.mapViewError.accept(MTMapViewError.locationAuthorizationDenied.errorDescription)
+            return
+        }
+    }
+}
+
+extension LocationInformationViewController: MTMapViewDelegate {
+    func mapView(_ mapView: MTMapView!, updateCurrentLocation location: MTMapPoint!, withAccuracy accuracy: MTMapLocationAccuracy) {
+        #if DEBUG
+        viewModel.currentLocation.accept(
+            MTMapPoint(
+                geoCoord: MTMapPointGeo(
+                    latitude: 37.394225,
+                    longitude: 127.11034
+                )
+            )
+        )
+        #else
+         viewModel.currentLocation.accept(location)
+        #endif
+    }
+    
+    func mapView(_ mapView: MTMapView!, finishedMapMoveAnimation mapCenterPoint: MTMapPoint!) {
+         viewModel.mapCenterPoint.accept(mapCenterPoint)
+    }
+    
+    func mapView(_ mapView: MTMapView!, selectedPOIItem poiItem: MTMapPOIItem!) -> Bool {
+         viewModel.selectPOIItem.accept(poiItem)
+        return false
+    }
+    
+    func mapView(_ mapView: MTMapView!, failedUpdatingCurrentLocationWithError error: Error!) {
+         viewModel.mapViewError.accept(error.localizedDescription)
+    }
+}
+
+extension Reactive where Base: MTMapView {
+    var setMapCenterPoint: Binder<MTMapPoint> {
+        return Binder(base) { base, point in
+            base.setMapCenter(point, animated: true)
+        }
+    }
+}
+
+extension Reactive where Base: LocationInformationViewController {
+    var presentAlert: Binder<String> {
+        return Binder(base) { base, message in
+            let alertController = UIAlertController(
+                title: "문제가 발생했어요",
+                message: message,
+                preferredStyle: .alert
+            )
+            
+            let action = UIAlertAction(
+                title: "확인",
+                style: .default,
+                handler: nil
+            )
+            
+            alertController.addAction(action)
+            
+            base.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
--- KakaoMapRxSwift/SceneDelegate.swift
+++ KakaoMapRxSwift/SceneDelegate.swift
@@ -10,12 +10,13 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    let viewModel = LocationInformationViewModel()
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = UINavigationController(rootViewController: LocationInformationViewController())
         window?.backgroundColor = .systemBackground
         window?.makeKeyAndVisible()
     }
